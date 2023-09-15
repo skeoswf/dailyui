bb0e967ddd17ec35ecfe0520e01a3a48369b3860
skeoswf <125794269+skeoswf@users.noreply.github.com>
2023-09-15 13:55:24 -0500

    Fix both buttons not animating

--- dailyui2/index.js
+++ dailyui2/index.js
@@ -1,6 +1,16 @@
-const buttons = document.querySelector("button"); 
+const button1 = document.querySelector("#card");
+const button2 = document.querySelector("#paypal")
 const choosetext = document.querySelector("#choosetext")
 
-buttons.addEventListener("click", function() {
-    choosetext.classList.add("choosetextPhase2")
+
+button1.addEventListener("click", function() {
+    choosetext.classList.add("choosetextPhase2");
+    button1.classList.add("buttonsPhase2")
+    button2.classList.add("buttonsPhase2")
+})
+
+button2.addEventListener("click", function() {
+    choosetext.classList.add("choosetextPhase2");
+    button1.classList.add("buttonsPhase2")
+    button2.classList.add("buttonsPhase2")
 })
