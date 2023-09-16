50b342d31793cc4f14df3c70240f7f27c8d7722c
skeoswf <125794269+skeoswf@users.noreply.github.com>
2023-09-16 10:39:32 -0500

    Add start of animation

--- dailyui2/index.js
+++ dailyui2/index.js
@@ -1,6 +1,7 @@
 const button1 = document.querySelector("#card");
 const button2 = document.querySelector("#paypal")
 const choosetext = document.querySelector("#choosetext")
+const allInformation = document.querySelector("#cardInformation")
 
 
 button1.addEventListener("click", function() {
@@ -13,4 +14,6 @@ button2.addEventListener("click", function() {
     choosetext.classList.add("choosetextPhase2");
     button1.classList.add("buttonsPhase2")
     button2.classList.add("buttonsPhase2")
+    allInformation.classList.add("cardInformationPhase2")
+    allInformation.style.visibility = "visible"
 })
