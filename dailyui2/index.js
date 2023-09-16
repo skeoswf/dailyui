c004e7d548a79b43cb0dd0810b1da9654764f427
skeoswf <125794269+skeoswf@users.noreply.github.com>
2023-09-16 12:27:04 -0500

    Fix paypal background color

--- dailyui2/index.js
+++ dailyui2/index.js
@@ -2,6 +2,7 @@ const button1 = document.querySelector("#card");
 const button2 = document.querySelector("#paypal")
 const choosetext = document.querySelector("#choosetext")
 const allInformation = document.querySelector("#cardInformation")
+const payPalIcon = document.querySelector("#payPalLink")
 
 
 
@@ -19,6 +20,4 @@ button2.addEventListener("click", function() {
     button1.classList.add("buttonsPhase2")
     button2.classList.add("buttonsPhase2")
     allInformation.classList.toggle("fadeOutClass")
-    // allInformation.style.visibility = "visible"
-    // allInformation.classList.add("cardInformationFade")
 })
